6369f8ff38a0cb7cd6f2c860c392f9084180d3ea
谢震 <46702380+hardshells@users.noreply.github.com>
2020-10-25 22:01:59 +0800

    11

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult View1()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
