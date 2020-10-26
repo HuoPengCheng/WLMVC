fc174ebc81f92155df5025958fe2316ca41ba5a7
家旺 张 <zjw991228@outlook.com>
2020-10-26 13:43:14 +0800

    15

---
+++ WebApplication1/Controllers/ZJWController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ZJWController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
